3cbb0b58bc76e36b385dfbaa2df105c6eda340b0
sampolgar@gmail.com <sampolgar@gmail.com>
2022-10-21 06:40:34 +1100

    update date

--- docs/blog.md
+++ docs/blog.md
@@ -1,3 +1,8 @@
 ## render markdown
 
 - use remark remark-html `npm install remark remark-html`
+
+## configure dates
+
+- use date-fns `npm install date-fns`
+- date format `https://date-fns.org/v2.16.1/docs/format`
--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,20 +1,26 @@
 import Layout from "../../components/layout";
 import { getAllPostsIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
