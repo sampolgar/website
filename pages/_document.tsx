3ff874832374d1e01ef4bb4f3252e0403c50ea83
Sam Polgar <sampolgar@gmail.com>
2023-01-07 21:15:14 +1100

    organizing folder structure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,3 @@
-// import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
-
-// class MyDocument extends Document {
-//   static async getInitialProps(
-//     ctx: DocumentContext
-//   ): Promise<DocumentInitialProps> {
-//     const initialProps = await Document.getInitialProps(ctx);
-
-//     return initialProps;
-//   }
-// }
-
-// export default MyDocument;
-
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
