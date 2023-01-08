1ea1913bc7d362080d51ff9b893ef1cef6c8e2f0
Sam Polgar <sampolgar@gmail.com>
2023-01-08 22:06:17 +1100

    adding a footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,48 +4,58 @@ import GitHub from '../../public/images/social/github-white.png';
 import Medium from '../../public/images/social/medium-white.png';
 import Twitter from '../../public/images/social/twitter-white.png';
 import LinkedIn from '../../public/images/social/linkedin-white.png';
+import Link from 'next/link';
 
 export function Footer() {
   return (
     <div className={styles.container}>
-      <div className={styles.navbar}>
+      <Link className={styles.link} href='https://github.com/sampolgar'>
         <Image
           className={styles.icon}
           src={GitHub}
           alt='picture of Sam Polgar Smiling'
-          width={66}
-          height={66}
+          width={20}
+          height={20}
           layout='responsive'
           priority={true}
         />
+      </Link>
+      <Link className={styles.link} href='https://sampolgar.medium.com'>
         <Image
           className={styles.icon}
           src={Medium}
           alt='picture of Sam Polgar Smiling'
-          width={66}
-          height={66}
+          width={20}
+          height={20}
           layout='responsive'
           priority={true}
         />
+      </Link>
+      <Link className={styles.link} href='https://twitter.com/0xsamp'>
         <Image
           className={styles.icon}
           src={Twitter}
           alt='picture of Sam Polgar Smiling'
-          width={66}
-          height={66}
+          width={20}
+          height={20}
           layout='responsive'
           priority={true}
         />
+      </Link>
+      <Link
+        className={styles.link}
+        href='https://www.linkedin.com/in/sampolgar/'
+      >
         <Image
           className={styles.icon}
           src={LinkedIn}
           alt='picture of Sam Polgar Smiling'
-          width={66}
-          height={66}
+          width={20}
+          height={20}
           layout='responsive'
           priority={true}
         />
-      </div>
+      </Link>
     </div>
   );
 }
