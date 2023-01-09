483327549501a81288c1117485e0699c4986849f
Sam Polgar <sampolgar@gmail.com>
2023-01-09 22:36:16 +1100

    refining styles

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,61 +1,62 @@
-import styles from './styles.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 import GitHub from '../../public/images/social/github-white.png';
+import LinkedIn from '../../public/images/social/linkedin-white.png';
 import Medium from '../../public/images/social/medium-white.png';
 import Twitter from '../../public/images/social/twitter-white.png';
-import LinkedIn from '../../public/images/social/linkedin-white.png';
-import Link from 'next/link';
+import styles from './styles.module.css';
 
 export function Footer() {
   return (
     <div className={styles.container}>
-      <Link className={styles.link} href='https://github.com/sampolgar'>
-        <Image
-          className={styles.icon}
-          src={GitHub}
-          alt='picture of Sam Polgar Smiling'
-          width={20}
-          height={20}
-          layout='responsive'
-          priority={true}
-        />
-      </Link>
-      <Link className={styles.link} href='https://sampolgar.medium.com'>
-        <Image
-          className={styles.icon}
-          src={Medium}
-          alt='picture of Sam Polgar Smiling'
-          width={20}
-          height={20}
-          layout='responsive'
-          priority={true}
-        />
-      </Link>
-      <Link className={styles.link} href='https://twitter.com/0xsamp'>
-        <Image
-          className={styles.icon}
-          src={Twitter}
-          alt='picture of Sam Polgar Smiling'
-          width={20}
-          height={20}
-          layout='responsive'
-          priority={true}
-        />
-      </Link>
-      <Link
-        className={styles.link}
-        href='https://www.linkedin.com/in/sampolgar'
-      >
-        <Image
-          className={styles.icon}
-          src={LinkedIn}
-          alt='picture of Sam Polgar Smiling'
-          width={20}
-          height={20}
-          layout='responsive'
-          priority={true}
-        />
-      </Link>
+      <div className={styles.link}>
+        <Link href='https://github.com/sampolgar'>
+          <Image
+            className={styles.icon}
+            src={GitHub}
+            alt='picture of Sam Polgar Smiling'
+            height={50}
+            width={50}
+            priority={true}
+          />
+        </Link>
+      </div>
+      <div className={styles.link}>
+        <Link href='https://sampolgar.medium.com'>
+          <Image
+            className={styles.icon}
+            src={Medium}
+            alt='picture of Sam Polgar Smiling'
+            height={50}
+            width={50}
+            priority={true}
+          />
+        </Link>
+      </div>
+      <div className={styles.link}>
+        <Link href='https://twitter.com/0xsamp'>
+          <Image
+            className={styles.icon}
+            src={Twitter}
+            alt='picture of Sam Polgar Smiling'
+            height={50}
+            width={50}
+            priority={true}
+          />
+        </Link>
+      </div>
+      <div className={styles.link}>
+        <Link href='https://www.linkedin.com/in/sampolgar'>
+          <Image
+            className={styles.icon}
+            src={LinkedIn}
+            alt='picture of Sam Polgar Smiling'
+            height={50}
+            width={50}
+            priority={true}
+          />
+        </Link>
+      </div>
     </div>
   );
 }
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,49 +1,35 @@
-import React from 'react';
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
-import { useRouter } from 'next/router';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 import { Footer } from './components/footer';
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - Sam Polgar',
+    };
+  },
+  faviconGlyph: '💡',
+  head: (
+    <>
+      <meta property='og:title' content='Sam Portfolio' />
+      <meta property='og:description' content='Sam Portfolio' />
+      <meta name='msapplication-TileColor' content='#ffffff' />
+      <meta name='theme-color' content='#ffffff' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta httpEquiv='Content-Language' content='en' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name='description' content='Sam Polgar`s Website' />
+      <meta name='og:description' content='Sam Polgar`s Website' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site:domain' content='https://twitter.com/0xsamp' />
+      <meta name='twitter:url' content='https://twitter.com/0xsamp' />
+      <meta name='apple-mobile-web-app-title' content='Nextra' />
+    </>
+  ),
   logo: (
     <>
       <span style={{ marginLeft: '2em', fontWeight: 800 }}>Sam Polgar</span>
     </>
   ),
-  head: () => {
-    const { title } = useConfig();
-    const { route } = useRouter();
-
-    return (
-      <>
-        <meta name='msapplication-TileColor' content='#ffffff' />
-        <meta name='theme-color' content='#ffffff' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta httpEquiv='Content-Language' content='en' />
-        <meta name='description' content='Sam Polgar`s Website' />
-        <meta name='og:description' content='Sam Polgar`s Website' />
-        <meta name='twitter:card' content='summary_large_image' />
-        {/* <meta name='twitter:image' content={socialCard} /> */}
-        <meta name='twitter:site:domain' content='https://twitter.com/0xsamp' />
-        <meta name='twitter:url' content='https://twitter.com/0xsamp' />
-        <meta
-          name='og:title'
-          content={title ? title + ' – Nextra' : 'Nextra'}
-        />
-        {/* <meta name='og:image' content={socialCard} /> */}
-        <meta name='apple-mobile-web-app-title' content='Nextra' />
-        <link rel='icon' href='/public/favicons/favicon.png' type='image/png' />
-        <link
-          rel='icon'
-          href='/public/favicons/favicon.png'
-          type='image/png'
-          media='(prefers-color-scheme: dark)'
-        />
-      </>
-    );
-  },
-  // navbar: {
-  //   component: <a href='https://nextra.site'>Nextra</a>,
-  // },
   project: {
     link: 'https://github.com/sampolgar/website',
   },
@@ -55,6 +41,11 @@ const config: DocsThemeConfig = {
   search: {
     placeholder: 'Site Search',
   },
+  darkMode: false,
+  feedback: {
+    
+  }
+  // primaryHue: { dark: 100, light: 200 },
 };
 
 export default config;
